9f9769ad60d49439879b26014766d9e9a0e48566
Meiosei <victorkauanx@gmail.com>
2025-09-25 17:04:52 -0300

    Item Routes Base Complete

--- db.js
+++ db.js
@@ -1,85 +1,109 @@
+import "dotenv/config";
+import { createClient } from "@supabase/supabase-js";
 
-import 'dotenv/config'
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.DATABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-
-
-
+const supabaseUrl = process.env.DATABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 async function getEstoque() {
-let { data: Estoque, error } = await supabase
-  .from('Estoque')
-  .select()
+  let { data: Estoque, error } = await supabase.from("Estoque").select();
 
   if (error) {
-    console.log(error)
-    return error
+    console.log(error);
+    return error;
   } else {
-  
     // console.log(Estoque)
-    return Estoque
-  }}
-
-// async function getEstoqueID(id) {
-// let { data: Estoque, error } = await supabase
-//   .from('Estoque')
-//   .select(`${id}`)
-
-//   if (error) {
-//     console.log(error)
-//   } else {
-//     console.log(Estoque)
-//   }}
-async function addItem(name,type){
-  const { data:Estoque, error } = await supabase
-  .from('Estoque')
-  .insert([
-    { nome_item: name, tipo_item: type ,adicionado_em: new Date().toISOString()}
-  ])
-  .select()
-   if (error) {
-    console.log(error)
-    return error
-  } else {
-    console.log(Estoque)
-    return console.log("Item adicionado com sucesso") 
+    return Estoque;
   }
 }
 
-// TODO arrumar o update
-async function updateItemByID(id, name,type){
-  const { data:Estoque, error } = await supabase
-  .from('Estoque')
-  .update({ 
-     nome_item: name,
-     tipo_item: type ,
-     adicionado_em: new Date().toISOString()})
-  .eq('tipo_item', id)
-  .select()
-   if (error) {
-    console.log(error)
-    return error
-  } else {
-    console.log(Estoque)
-    return console.log("Item atualizado com sucesso ") 
+async function getEstoqueByID(id) {
+  try {
+    const { data: Estoque, error } = await supabase
+      .from("Estoque")
+      .select("*")
+      .eq("id", `${id}`)
+      .single();
+
+    if (error) {
+      console.log(false);
+      if (error.code === "PGRST116") {
+        return error;
+      }
+    } else {
+      console.log(Estoque);
+      return Estoque;
+    }
+  } catch (error) {
+    console.log(error);
+    return error;
   }
 }
 
-async function deleteItemByID(id){
-  const { error } = await supabase
-  .from('Estoque')
-  .delete()
-  .eq('id', id)
-    if (error) { 
-    console.log(error)
-    return error
+async function addItem(name, type) {
+  const { data: Estoque, error } = await supabase
+    .from("Estoque")
+    .insert([
+      {
+        nome_item: name,
+        tipo_item: type,
+        adicionado_em: new Date().toISOString(),
+      },
+    ])
+    .select();
+  if (error) {
+    console.log(error);
+    return error;
   } else {
-    return console.log("Item deletado com sucesso")
-     }
+    console.log(Estoque);
+    return console.log("Item adicionado com sucesso");
+  }
 }
 
-  export {supabase, getEstoque,addItem, deleteItemByID, updateItemByID}
+async function updateItemByID(id, name, type) {
+  const { data: Estoque, error } = await supabase
+    .from("Estoque")
+    .update({ nome_item: name, tipo_item: type })
+    .eq("id", id)
+    .select();
+
+  if (error) {
+    console.log(error);
+    return error;
+  } else {
+    if (Estoque.length === 0) {
+      return false;
+    }
+    console.log(Estoque);
+    return true;
+  }
+}
+
+async function deleteItemByID(id) {
+  try {
+    const { error } = await supabase.from("Estoque").delete().eq("id", id);
+
+    if (error) {
+      console.log(error);
+      if (error.code === "PGRST116") {
+        return false;
+      } else {
+        return error;
+      }
+    } else {
+      return true;
+    }
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+export {
+  supabase,
+  getEstoque,
+  addItem,
+  deleteItemByID,
+  updateItemByID,
+  getEstoqueByID,
+};
--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,50 +1,105 @@
-import express from 'express'
-import { getEstoque, addItem, deleteItemByID, updateItemByID} from '../db.js'
-const router = express.Router()
+import express from "express";
+import {
+  getEstoque,
+  addItem,
+  deleteItemByID,
+  updateItemByID,
+  getEstoqueByID,
+} from "../db.js";
+const router = express.Router();
 
 //OK-------------------------------------------------
-router.get('/item', async (req,res)=>{
-    const message = await getEstoque()
-   res.status(200).json(message)
-})
-
-
-
-router.get('/item/:id', (req,res)=>{
-    res.status(200)
-    res.send('Rota pública').json({message: 'Rota pública'})
-})
-
-
+router.get("/item", async (req, res) => {
+  try {
+    const message = await getEstoque();
+    res.status(200).json(message);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Erro ao buscar os itens" });
+  }
+});
 //OK-------------------------------------------------
-router.post('/item', (req,res)=>{
-    const {name, type, userId} = req.body
-    if(!name){
-        return res.status(422).json({message: 'O nome do item é obrigatório'})
+router.get("/item/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const message = await getEstoqueByID(id);
+    console.log(id);
+    if (!id) {
+      return res.status(422).json({ message: "ID do item é obrigatório" });
     }
-    if(!type){
-        return res.status(422).json({message: 'O tipo do item é obrigatório'})
+    if (message.code === "PGRST116") {
+      message.exist = false;
+      return res
+        .status(404)
+        .json({ message: "Item não encontrado", exist: false });
+    } else {
+      message.exist = true;
+      res.status(200).json(message);
     }
-    else{
-        addItem(name, type, userId)
-        return res.status(201).json({message: 'Item Criado com Sucesso!'})
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Erro ao buscar o item" });
+  }
+});
+//OK-------------------------------------------------
+router.post("/item", (req, res) => {
+  try {
+    const { name, type, userId } = req.body;
+    if (!name) {
+      return res.status(422).json({ message: "O nome do item é obrigatório" });
     }
-})
+    if (!type) {
+      return res.status(422).json({ message: "O tipo do item é obrigatório" });
+    } else {
+      addItem(name, type, userId);
+      return res.status(201).json({ message: "Item Criado com Sucesso!" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Erro ao criar o item" });
+  }
+});
+//MORE OR LESS OK, Item já não existe precisa ser consertado-------------------------------------------------
+router.delete("/item/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const resBD = await deleteItemByID(id);
+    if (!id) {
+      return res.status(422).json({ message: "ID do item é obrigatório" });
+    }
+    if (!resBD) {
+      return res
+        .status(404)
+        .json({ message: "Item não encontrado", exist: false });
+    } else {
+      console.log(resBD);
+      res.status(200).json({ message: "Item deletado com sucesso" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Erro ao deletar o item" });
+  }
+});
+//OK-------------------------------------------------
+router.put("/item/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { name, type } = req.body;
+    if (!name || !type) {
+      return res.status(422).json({ message: "Nome e Tipo são obrigatórios" });
+    }
+    const updateStatus = await updateItemByID(id, name, type);
+    if (!updateStatus) {
+      return res
+        .status(404)
+        .json({ message: "Item não encontrado", exist: false });
+    } else {
+      console.log(updateStatus);
+      res.status(200).json({ message: "Item atualizado com sucesso" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Erro ao atualizar o item" });
+  }
+});
 
-
-router.delete('/item/:id', async(req,res)=>{
-    const id = req.params.id
-    await deleteItemByID(id)
-    res.status(200).json({message: 'Item deletado com sucesso'})
-})
-
-// TODO arrumar put
-router.put('/item/:id', async (req,res)=>{
-    const id = req.params.id
-    const name = req.params.name
-    const type = req.params.type
-    await updateItemByID(id, name,type)
-    res.status(200).json({message: 'Item atualizado com sucesso'})
-})
-    
-export default router
+export default router;
